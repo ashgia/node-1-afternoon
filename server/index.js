d74fed10cdbfaa2df29396d18020e3331f594c3f
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-08-15 16:22:00 -0500

    completed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const { json } = require("body-parser");
+const mc = require(__dirname + "/controllers/messages_controller");
 const port = 3000;
 
 const app = express();
 
 app.use(json());
+app.use( express.static( __dirname + '/../public/build' ) );
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
